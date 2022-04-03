45b7c52d11031e01231a7c59d8ad456b0b57ce05
Sinchan <sinchanmandal659@gmail.com>
2022-04-04 00:59:55 +0530

    heroku creating

--- app.js
+++ app.js
@@ -78,9 +78,17 @@ app.get("*", (req, res)=>{
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port,function(){
+  console.log("Server started on port successfully");
+});
+/*
 app.listen(3000, function(){
     console.log("server has started");
-});
+});*/
 
 
 
